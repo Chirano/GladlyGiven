9f89ee89f904bf02a0a54837236ddf1d4037d8c7
Chirano <clarissachirano13@gmail.com>
2024-03-07 14:10:06 +0000

    added services

---
+++ dotNetSprint2GladyGivenWebAPI/GladlyGiven/Data/ApplicationContextDb.cs
@@ -0,0 +1,17 @@
+﻿using GladyGivenWebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladyGivenWebAPI.Data
+{
+    public class ApplicationContextDb : DbContext 
+    {
+        public ApplicationContextDb(DbContextOptions<ApplicationContextDb> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+
+        public DbSet<Service> Service { get; set; }
+    }
+}
