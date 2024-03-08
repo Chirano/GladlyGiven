855205e118b60267a1a54aa6a8ecd8af9caa1c54
Lia Araruna <105323436+liaararuna@users.noreply.github.com>
2024-03-08 11:40:27 +0000

    Added comments and exceptions to CostSupport

--- dotNetSprint2GladyGivenWebAPI/GladlyGiven/Controllers/CostSupportController.cs
+++ dotNetSprint2GladyGivenWebAPI/GladlyGiven/Controllers/CostSupportController.cs
@@ -7,7 +7,6 @@ using GladlyGiven.Services;
 using GladyGivenWebAPI.Data;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GladlyGiven.Controllers
 {
@@ -15,39 +14,97 @@ namespace GladlyGiven.Controllers
     [Route("api/v1/[controller]")]
     public class CostSupportController : ControllerBase
     {
-        private readonly CostSupportService costService;
+        private readonly CostSupportService costSupportService;
 
         public CostSupportController(ApplicationContextDb context)
         {
-            costService = new CostSupportService(context);
+            costSupportService = new CostSupportService(context);
         }
 
+        /// <summary>
+        /// Retrieves a paginated list of all cost support records.
+        /// </summary>
+        /// <param name="page">The page number (one-based) of the result set to retrieve (default is 1).</param>
+        /// <param name="pageSize">The number of elements per page (default is 5).</param>
+        /// <returns>
+        /// Returns an ActionResult<IEnumerable<CostSupportDTO>> representing the HTTP response.
+        /// If cost supports are found, it returns a response with HTTP 200 (OK) status code and the paginated list of cost supports.
+        /// If no cost supports are found, it returns a response with HTTP 204 (No Content) status code.
+        /// </returns>
         [HttpGet("/costsupports")]
-        public async Task<ActionResult<IEnumerable<CostSupportDTO>>> GetAllCostSupports()
+        public async Task<ActionResult<IEnumerable<CostSupportDTO>>> GetAllCostSupports(int page = 1, int pageSize = 5)
         {
-            List<CostSupportDTO> costSupports = await costService.FindAllCostSupports();
+            List<CostSupportDTO> costSupports = await costSupportService.FindAllCostSupports();
 
             if (costSupports != null && costSupports.Count > 0)
-                return Ok(costSupports);
+                return Ok(costSupports.Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList());
 
             return NoContent();
         }
 
+        /// <summary>
+        /// Retrieves a paginated list of cost supports associated with a specific user based on their <paramref name="userId"/>.
+        /// </summary>
+        /// <param name="userId">The unique identifier of the user for whom cost supports are to be retrieved.</param>
+        /// <param name="page">The page number (one-based) of the result set to retrieve (default is 1).</param>
+        /// <param name="pageSize">The number of elements per page (default is 5).</param>
+        /// <returns>
+        /// Returns an ActionResult<IEnumerable<CostSupportDTO>> representing the HTTP response.
+        /// If cost supports are found, it returns a response with HTTP 200 (OK) status code and the paginated list of cost supports.
+        /// If no cost supports are found, it returns a response with HTTP 204 (No Content) status code.
+        /// </returns>
         [HttpGet("mycostsupports/{id}")]
-        public async Task<ActionResult<IEnumerable<CostSupportDTO>>> GetAllCostSupportsById(int userId)
+        public async Task<ActionResult<IEnumerable<CostSupportDTO>>> GetAllCostSupportsByUserId(int userId, int page = 1, int pageSize = 5)
         {
-            List<CostSupportDTO> costSupports = await costService.FindAllCostSupports();
+            List<CostSupportDTO> costSupports = await costSupportService.FindAllCostSupports();
 
             if (costSupports != null && costSupports.Count > 0)
-                return Ok(costSupports);
+                return Ok(costSupports.Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList());
 
             return NoContent();
         }
 
-        [HttpGet("costsupport/id")]
+        /// <summary>
+        /// Retrieves a paginated list of cost supports with the specified <paramref name="costSupportStatus"/>.
+        /// </summary>
+        /// <param name="costSupportStatus">The status of the cost supports to be retrieved.</param>
+        /// <param name="page">The page number (one-based) of the result set to retrieve (default is 1).</param>
+        /// <param name="pageSize">The number of elements per page (default is 5).</param>
+        /// <returns>
+        /// Returns an ActionResult<IEnumerable<CostSupportDTO>> representing the HTTP response.
+        /// If cost supports are found, it returns a response with HTTP 200 (OK) status code and the paginated list of cost supports.
+        /// If no cost supports are found, it returns a response with HTTP 204 (No Content) status code.
+        /// </returns>
+        [HttpGet("status/{costSupportStatus}")]
+        public async Task<ActionResult<IEnumerable<CostSupportDTO>>> GetAllCostSupportsByStatus(int costSupportStatus, int page = 1, int pageSize = 5)
+        {
+            List<CostSupportDTO> costSupports = await costSupportService.FindAllCostSupportsByStatus(costSupportStatus);
+
+            if (costSupports != null && costSupports.Count > 0)
+                return Ok(costSupports.Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToList());
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Retrieves the cost support record with the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The unique identifier of the cost support record to be retrieved.</param>
+        /// <returns>
+        /// Returns an ActionResult<CostSupportDTO> representing the HTTP response.
+        /// If the cost support record is found, it returns a response with HTTP 200 (OK) status code and the cost support details.
+        /// If the record is not found, it returns a response with HTTP 204 (No Content) status code.
+        /// </returns>
+        [HttpGet("{id}")]
         public async Task<ActionResult<CostSupport>> GetCostSupport(int id)
         {
-            CostSupportDTO costSupport = await costService.FindCostSupport(id);
+            CostSupportDTO costSupport = await costSupportService.FindCostSupport(id);
 
             if (costSupport != null)
                 return Ok(costSupport);
@@ -55,10 +112,19 @@ namespace GladlyGiven.Controllers
             return NoContent();
         }
 
+        /// <summary>
+        /// Creates a new cost support record using the provided <paramref name="cost"/>.
+        /// </summary>
+        /// <param name="cost">The costsupportDTO containing details for the cost support record.</param>
+        /// <returns>
+        /// Returns an ActionResult<CostSupportDTO> representing the HTTP response.
+        /// If the cost support record is created successfully, it returns a response with HTTP 200 (OK) status code and the created cost support details.
+        /// If the creation fails or there is no content to return, it returns a response with HTTP 204 (No Content) status code.
+        /// </returns>
         [HttpPost("/costsupport")]
         public async Task<ActionResult<CostSupportDTO>> CreateCostSupport(CostSupportDTO cost)
         {
-            CostSupportDTO costSupport = await costService.CreateCostSupport(cost);
+            CostSupportDTO costSupport = await costSupportService.CreateCostSupport(cost);
 
             if (costSupport != null)
                 return Ok(costSupport);
@@ -66,19 +132,34 @@ namespace GladlyGiven.Controllers
             return NoContent();
         }
 
-        /*
+        /// <summary>
+        /// Updates the status of the cost support record with the specified <paramref name="id"/>.
+        /// </summary>
+        /// <param name="id">The unique identifier of the cost support record to be updated.</param>
+        /// <param name="cost">The costsupportDTO containing updated details for the cost support record.</param>
+        /// <param name="costSupportStatus">The updated status for the cost support record.</param>
+        /// <returns>
+        /// Returns an ActionResult<CostSupportDTO> representing the HTTP response.
+        /// If the cost support record is updated successfully, it returns a response with HTTP 200 (OK) status code.
+        /// If the request is invalid, it returns a response with HTTP 400 (Bad Request) status code.
+        /// </returns>
         [HttpPut("/costsupport/id")]
-        public async Task<ActionResult<CostSupportDTO>> UpdateCostSupport(int id, CostSupportDTO cost)
+        public async Task<ActionResult<CostSupportDTO>> UpdateCostSupport(int id, CostSupportDTO cost, int costSupportStatus)
         {
-            if (cost.Id != id)
+            if(cost.Id != id)
             {
                 return BadRequest();
             }
 
-            CostSupportDTO updatedCost = await costService.UpdateCostSupport(cost);
+            if(costSupportStatus == 0) //Status: waiting validation.
+            {
+                return BadRequest();
+            }
+
+            CostSupportDTO updatedCost = await costSupportService.UpdateCostSupport(cost, costSupportStatus);
 
             return Ok();
-        }*/
+        }
     }
 }
 
--- dotNetSprint2GladyGivenWebAPI/GladlyGiven/Services/CostSupportService.cs
+++ dotNetSprint2GladyGivenWebAPI/GladlyGiven/Services/CostSupportService.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using GladlyGiven.Exceptions;
 using GladlyGiven.Models;
 using GladyGivenWebAPI.Data;
+using GladyGivenWebAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace GladlyGiven.Services
@@ -24,7 +26,7 @@ namespace GladlyGiven.Services
 
            if (costSupports == null)
            {
-               return null; //TODO: alterar para exceção TableIsEmpty();
+                throw new EntityDoesntExistException("Table CostSupports is empty", "CostSupport", "FindAllCostSupports()");
            }
 
            List<CostSupportDTO> costSupportDTOs = new List<CostSupportDTO>();
@@ -43,17 +45,16 @@ namespace GladlyGiven.Services
        /// </summary>
        /// <param name="userId">Service provider id</param>
        /// <returns>List of all cost supports requested by a service provider</returns>
-           public async Task<List<CostSupportDTO>> FindAllCostSupportsById(int userId)
-           {
+       public async Task<List<CostSupportDTO>> FindAllCostSupportsByUserId(int userId)
+       {
                List<CostSupport> costSupports = await context.CostSupports
                    .Where(sp => sp.Id == userId)
                    .ToListAsync();
 
                if (costSupports == null)
                {
-                   //TODO: alterar para exceção ThereIsNoCostSupportException();
-                   return null;
-               }
+                    throw new EntityDoesntExistException("There's no CostSupports for this user", "CostSupport", "FindAllCostSupportsByUserId()");
+                }
 
                List<CostSupportDTO> costSupportDTOs = new List<CostSupportDTO>();
 
@@ -66,6 +67,37 @@ namespace GladlyGiven.Services
                return costSupportDTOs;
            }
 
+        /// <summary>
+        /// Retrieves a list of cost supports with the specified <paramref name="costSupportStatus"/>.
+        /// </summary>
+        /// <param name="costSupportStatus">The status of the cost supports to be retrieved.</param>
+        /// <returns>
+        /// Returns a Task<List<CostSupportDTO>" representing the asynchronous operation.
+        /// The task result is a list of CostSupportDTO representing the cost supports with the specified status.
+        /// Throws an <see cref="EntityDoesntExistException"/> if no cost supports are found for the given status.
+        /// </returns>
+        public async Task<List<CostSupportDTO>> FindAllCostSupportsByStatus(int costSupportStatus)
+            {
+               List<CostSupport> costSupports = await context.CostSupports
+                   .Where(sp => (int)sp.Status == costSupportStatus)
+                   .ToListAsync();
+
+               if (costSupports == null)
+               {
+                    throw new EntityDoesntExistException("There's no CostSupports for this status", "CostSupport", "FindAllCostSupportsByStatus()");
+                }
+
+               List<CostSupportDTO> costSupportDTOs = new List<CostSupportDTO>();
+
+               foreach (CostSupport cost in costSupports)
+               {
+                   CostSupportDTO costSupportDTO = new CostSupportDTO(cost);
+                   costSupportDTOs.Add(costSupportDTO);
+               }
+
+               return costSupportDTOs;
+            }
+
            /// <summary>
            /// Find a specific cost support by its id
            /// </summary>
@@ -76,7 +108,7 @@ namespace GladlyGiven.Services
                var costSupport = await context.CostSupports.FirstOrDefaultAsync(cs => cs.Id == id);
                if (costSupport == null)
                {
-                   throw new NotImplementedException(); //TODO: alterar para exceção EntityDoesntExistsException();
+                   throw new EntityDoesntExistException("Table CostSupports is empty", "CostSupport", "FindCostSupport()");
                }
 
                CostSupportDTO costSupportDTO = new CostSupportDTO(costSupport);
@@ -88,7 +120,7 @@ namespace GladlyGiven.Services
            /// Service provider requests a cost support.
            /// </summary>
            /// <param name="cost">Cost support DTO</param>
-           /// <returns>costSupportDTO</returns>
+           /// <returns>costSupportDTO created</returns>
            public async Task<CostSupportDTO> CreateCostSupport(CostSupportDTO cost)
            {
                var costSupport = await context.CostSupports.FirstOrDefaultAsync(cs => cs.Id == cost.Id);
@@ -113,17 +145,39 @@ namespace GladlyGiven.Services
            }
 
 
-           /*
-           /// <summary>
-           /// Administrator validates a cost support requested.
-           /// </summary>
-           /// <param name="cost">Cost support to be validated</param>
-           /// <returns>Cost support status updated</returns>
-           public async Task<CostSupportDTO> UpdateCostSupport(CostSupportDTO cost)
-           {
 
-           }*/
+        /// <summary>
+        /// Administrator validates (approves or rejects) a cost support requested.
+        /// </summary>
+        /// <param name="cost">Cost support to be validated</param>
+        /// <returns>Cost support status updated</returns>
+        public async Task<CostSupportDTO> UpdateCostSupport(CostSupportDTO cost, int costSupportStatus)
+        {
+            var costSupport = await context.CostSupports.FirstOrDefaultAsync(cs => cs.Id == cost.Id);
 
-       }
+            if(costSupport == null)
+            {
+                throw new EntityDoesntExistException("CostSupport was not found", "CostSupport", "UpdateCostSupport()");
+            }
+
+            if (costSupportStatus == 1)
+            {
+                costSupport.Status = Enums.CostSupportStatus.APPROVED;
+                context.CostSupports.Entry(costSupport).State = EntityState.Modified;
+            }
+
+            if (costSupportStatus == 2)
+            {
+                costSupport.Status = Enums.CostSupportStatus.REJECTED;
+                context.CostSupports.Entry(costSupport).State = EntityState.Modified;
+            }
+
+            await context.SaveChangesAsync();
+
+            CostSupportDTO dTO = new CostSupportDTO(costSupport);
+
+            return dTO;
+        }
    }
+}
 
