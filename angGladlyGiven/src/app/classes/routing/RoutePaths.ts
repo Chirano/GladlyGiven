06886999b1233d7e9581dd5191f4ff8d3e2ac84f
Hugo Lopes <hugoleo74@gmail.com>
2024-03-12 14:24:48 +0000

    Update RoutePaths.ts

--- angGladlyGiven/src/app/classes/routing/RoutePaths.ts
+++ angGladlyGiven/src/app/classes/routing/RoutePaths.ts
@@ -28,5 +28,10 @@ export const RouterPaths = {
     ViewServiceRequest: "view-service-request",
 
     ViewDonation: "view-donation",
+<<<<<<< Updated upstream
     ViewAdminListCostsupport : "view-admin/costsupportlist"
+=======
+
+    ViewListServiceRequest: "view-list-servicerequest"
+>>>>>>> Stashed changes
 }
