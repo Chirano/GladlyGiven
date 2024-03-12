818abb31237270f695daedea4fedfeb140ed9053
Hugo Lopes <hugoleo74@gmail.com>
2024-03-12 15:33:12 +0000

    Update view-list-servicerequest.component.ts and html

--- angGladlyGiven/src/app/components/views/view-list-servicerequest/view-list-servicerequest.component.ts
+++ angGladlyGiven/src/app/components/views/view-list-servicerequest/view-list-servicerequest.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ServiceRequest } from 'src/app/classes/ServiceRequest';
+import { ServiceRequestService } from 'src/app/services/service-request/service-request.service';
+
 
 @Component({
   selector: 'app-view-list-servicerequest',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-list-servicerequest.component.scss']
 })
 export class ViewListServicerequestComponent {
+
+  serviceRequests: ServiceRequest[] = [];
+
+  constructor(
+    private serviceRequestService : ServiceRequestService) {}
+  
+    ngOnInit(): void {
+      this.getServiceRequests(); // Chama o método ao iniciar o componente
+    }
+
+
+    //GET: returns all costs supports saved in database.
+    getServiceRequests() : void {
+      this.serviceRequestService.getServiceRequest().subscribe(serviceRequests => this.serviceRequests = serviceRequests)
+    }
 
 }
