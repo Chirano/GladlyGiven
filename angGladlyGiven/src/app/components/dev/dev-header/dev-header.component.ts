73d068abf0af6f4be606f77d9486de81180af9e1
tbarracha <ti.barracha@gmail.com>
2024-03-12 16:38:15 +0000

    added dev-header with our links

--- angGladlyGiven/src/app/components/dev/dev-header/dev-header.component.ts
+++ angGladlyGiven/src/app/components/dev/dev-header/dev-header.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterPaths } from 'src/app/classes/routing/RoutePaths';
+import { EventManagerService } from 'src/app/services/events/event-manager.service';
 
 @Component({
   selector: 'app-dev-header',
   templateUrl: './dev-header.component.html',
   styleUrls: ['./dev-header.component.scss']
 })
-export class DevHeaderComponent {
 
+export class DevHeaderComponent {
+  toSignIn() {
+    EventManagerService.OnRouteEvent.emit(RouterPaths.SignIn);
+  }
+
+  toSignUp() {
+    EventManagerService.OnRouteEvent.emit(RouterPaths.SignUp);
+  }
+
+  toContact() {
+    EventManagerService.OnRouteEvent.emit(RouterPaths.ViewContact);
+  }
+
+  toFaq() {
+    EventManagerService.OnRouteEvent.emit(RouterPaths.ViewFaq);
+  }
+
+  toServiceRequest() {
+    EventManagerService.OnRouteEvent.emit(RouterPaths.ViewServiceRequest);
+  }
 }
